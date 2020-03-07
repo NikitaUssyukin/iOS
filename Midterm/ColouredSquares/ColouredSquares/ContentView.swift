59354609d78c96852762815e394051813009f1be
Yakuz4 <47651580+Yakuz4@users.noreply.github.com>
2020-03-07 18:13:23 +0600

    Add files via upload

---
+++ Midterm/ColouredSquares/ColouredSquares/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColouredSquares
+//
+//  Created by Yakuza on 07/03/2020.
+//  Copyright © 2020 Nikita Ussyukin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Я дебил, зря не ходил на лекции.\n ))))))))))")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
